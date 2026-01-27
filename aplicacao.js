eb7f41b39dbf47514b1418912d26e06782f84e65
Dina-Simoes <dayaala@proton.me>
2026-01-27 14:05:10 +0000

    Injeção dinâmica HTML
    Inicio da estruturação da injeção dinâmica HTML

---
+++ aplicacao.js
@@ -0,0 +1,29 @@
+// para apagar mais tarde
+console.log("Aplicação iniciada.")
+
+const content = document.getElementById("content");
+
+function loadHeader() {
+    
+}
+
+function loadFooter() {}
+
+function loadLeads() {}
+
+function loadClientes() {}
+
+// as 2 funções seguintes serão terminadas em projetos futuros
+function loadProjetos() {}
+
+function loadTarefas() {}
+
+
+function loadLogin() {}
+
+function loadDetalhes() {}
+
+// mais tarde, completar as seguintes funções
+function saveLead() {}
+
+function saveCliente() {}
