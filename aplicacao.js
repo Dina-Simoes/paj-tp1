0a91a2794233bfc6b51e2680850f56eac8612f0d
Dina-Simoes <dayaala@proton.me>
2026-01-27 15:05:38 +0000

    HTML: injeção dinamica
    Continuação Injeção dinamica HTML
    Alteração nos ficheiros dashboard.html e aplicacao.js

--- aplicacao.js
+++ aplicacao.js
@@ -4,14 +4,39 @@ console.log("Aplicação iniciada.")
 const content = document.getElementById("content");
 
 function loadHeader() {
-    
+    const headerDiv = document.getElementById("header");
+
+    headerDiv.innerHTML = `
+    <header>
+        <h1>Customer relationship Management</h1>
+        <p>Utilizador: nome</p>
+
+
+    </header>
+    `
 }
 
-function loadFooter() {}
+function loadFooter() {
+    const footerDiv = document.getElementById("footer");
 
-function loadLeads() {}
+    footerDiv.innerHTML = `
+    <p>@2026 all right reserved</p>
+    `
+}
 
-function loadClientes() {}
+function loadLeads() {
+    content.innerHTML = `
+    <h2>Leads</h2>
+    <p>Lista de Leads</p>
+    `
+}
+
+function loadClientes() {
+    content.innerHTML = `
+    <h2>Clientes</h2>
+    <p>Lista de Clientes</p>
+    `
+}
 
 // as 2 funções seguintes serão terminadas em projetos futuros
 function loadProjetos() {}
